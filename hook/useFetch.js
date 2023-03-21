123406473bd26d86b01894fe78f3c38f653a07e2
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-03-21 17:39:36 +0200

    key was removed

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from "@env"; //"react-native-dotenv";
-// import * as dotenv from "dotenv";
-// dotenv.config();
+import { RAPID_API_KEY } from "@env";
 
-const rapidApiKey = RAPID_API_KEY; //"98de267001mshba747029c726229p182013jsnece8e9081873";
+const rapidApiKey = RAPID_API_KEY;
 
 export const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
